002742c16e1173f8e30cb2114c25b107872011e7
ClementLancien <clement.lancien@gmail.com>
2017-07-25 11:08:20 +0200

    handsontable

--- geneulike/webapp/app/scripts/geneulike.js
+++ geneulike/webapp/app/scripts/geneulike.js
@@ -2189,6 +2189,25 @@ app.controller('createCtrl',
     function ($scope, $rootScope, $routeParams, $location, Auth, User,Upload,ngDialog) {
 
 
+        var data = [
+                        ["", "Ford", "Volvo", "Toyota", "Honda"],
+                        ["2016", 10, 11, 12, 13],
+                        ["2017", 20, 11, 14, 13],
+                        ["2018", 30, 15, 12, 13]
+                    ];
+
+        var container = document.getElementById('table');
+        var hot = new Handsontable(container, {
+            data: data,
+            rowHeaders: true,
+            colHeaders: true
+        });
+
+
+
+
+
+
 //         $scope.user = null;
 
 //         User.get({'uid': $routeParams['id']}).$promise.then(function(data){
